7331caa4ddd29a4a1820f3dfde81d2e363ff16e6
Uvini Wickramaarachchi <uvini@uvini.local>
2026-07-13 13:44:36 +0530

    Intial React project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h1> 🐾 Paws and Homes</h1>
+      <h2>Welcome to our Pet Adoption Portal</h2>
+      <p>Built using React + Asgardeo + Choreo</p>
+    </div>
+  );
+}
+
+export default Home;
