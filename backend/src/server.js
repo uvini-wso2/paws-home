518496b014e3d5ceef98f0b1c1e4ad04fa72ca85
Uvini Wickramaarachchi <uvini@uvini.local>
2026-07-31 11:55:07 +0530

    fix cors issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,16 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.use("/api/applications", applicationRoutes);
