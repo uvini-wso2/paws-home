d33ba18ff2f71571e3268d2306a930ec7beb7fb0
Uvini Wickramaarachchi <uvini@uvini.local>
2026-07-31 12:21:02 +0530

    fix db connection for production

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -14,9 +14,7 @@ export const getDB = async () => {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      port: process.env.DB_PORT, 
-      waitForConnections: true,
-      connectionLimit: 10,
+      port: process.env.DB_PORT,
     });
 
     console.log("✅ DB connected");
