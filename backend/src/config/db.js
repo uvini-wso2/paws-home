ac33b04041bc8a4e2de356a2636c90162b01db09
Uvini Wickramaarachchi <uvini@uvini.local>
2026-07-31 11:03:46 +0530

    Added DB connection for Railway

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -14,6 +14,9 @@ export const getDB = async () => {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      port: process.env.DB_PORT, 
+      waitForConnections: true,
+      connectionLimit: 10,
     });
 
     console.log("✅ DB connected");
